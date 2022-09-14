75281f9feb5157bc6b969b088f50de93f9e5c6d7
Saidakmal0816 <saidakmal468@gmail.com>
2022-09-14 14:43:31 +0500

    3.2

---
+++ main.js
@@ -0,0 +1,225 @@
+//   1
+var number = 4
+if (number> 0 ){
+ console.log("Bu musbat son");
+}
+    
+    
+    
+    // 2
+
+    
+    var number =16
+    if (number % 2 == 0 ){
+    console.log("Bu juft son");
+    } else{
+    console.log("Bu toq son");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    //   3
+    
+    var number = 14
+    if (number % 2 == 0 ){ 
+    console.log("Bu toq son");
+    }else{
+    console.log("Bu toq son");
+    }
+
+    // 1
+
+
+var son = 3
+if(son > 0){
+    console.log("son musbat");
+}else{
+    console.log("son manfiy")
+}
+
+// 2;3
+
+
+var son = 105
+if(son % 2 == 0){
+console.log("son juft")
+}if(son % 2 ==1){
+    console.log("son toq")
+}
+
+
+// 4
+
+
+var son = 2
+var son = 4
+if(son > 0){
+    console.log("ikkala son musbat")
+
+}if(son < 0){
+    console.log("bitta son manfiy")
+}
+
+
+// 5
+
+
+var a = 12
+var b = 33 
+var c = 40
+if(a>b&&b>c){
+    console.log("a son eng katta");
+    
+}else{
+    console.log("c son eng katta")
+}
+
+
+// 6
+
+
+var a = 12
+var b = 33 
+var c = 4
+if(a<b&&b>c){
+    console.log("b son ortada")
+}
+
+
+// 7
+
+var a = 12
+var b = 32
+if(a %2 ==0&&b %2 ==0){
+    console.log("ikkalasi juft");
+}else if(a %2 ==1 && b %2 ==1){
+    console.log("ikkalasi toq")
+}
+
+
+// 8
+
+var a = 12
+var b = 33 
+var c = 40
+var d = 22
+
+if(a > 0 && b  > 0 && c >0 && d >0){
+console.log("hammasi musbat");
+}
+
+// 10
+
+// var du =1
+// var se =2
+// var ch =3
+// var pa =4
+// var ju =5
+// var sh =6
+// var ya =7
+// if()
+
+
+11
+
+var mobiuz=97
+var ucell=93
+if(mobiuz==97){
+    console.log("mobiuz")
+
+}if(ucell==93){
+    console.log("ucell")
+}
+
+
+12
+
+var a=4
+// var b=6
+if(a>0){
+    console.log(a+1)
+
+}
+if(a<0){
+    console.log(a-1)
+}
+
+
+14
+
+var a=20
+var b=3
+if(a>b){
+    console.log("a son katta");
+}if(b>a){
+    console.log("b son katta")
+}
+
+
+15
+
+
+var a = 3
+var b = 6
+var c = 9
+if(a<c&&b<c){
+    console.log("c son katta")
+
+}if(a<b&&b>c){
+    console.log("b son katta")
+
+}if(a>b&&a>c){
+    console.log("a son katta")
+}
+
+
+
+16
+
+
+var a = 3
+var b = 2
+var c = 9
+if(a>c&&b>c){
+    console.log("c son kichik")
+
+}if(a>b&&b<c){
+    console.log("b son kichik")
+
+}if(a<b&&a<c){
+    console.log("a son kichik")
+}
+
+
+18
+
+
+"baxoni kiriting"
+var baxo=2
+
+if(baxo==5){
+    console.log("5mln")
+}
+if(baxo==4){
+    console.log("4mln")
+}
+if(baxo==3){
+    console.log("3mln")
+}
+if(baxo==2){
+    console.log("2mln")
+}
+if(baxo==1){
+    console.log("1mln")
+}
+    
+    
+    
+    
+    
+   
